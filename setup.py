397eb2066340f1aab672b6db273b491115ee0475
Lee Supe <lain.proliant@gmail.com>
2013-03-26 10:25:13 -0400

    Added setup.py script for installing webmail package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (
+      name = "Webmail Client Tools",
+      version = "0.1",
+      description = "Intuitive tools and APIs for interacting with email from an IMAP inbox.",
+      author = "Lee Supe",
+      author_email = "lain.proliant@gmail.com",
+      url = "http://github.com/lainproliant/python3-webmail",
+      packages = ['webmail']
+      )
+
